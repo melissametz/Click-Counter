70734a8a3219ba8d2aef3e78daa2190a941b28bf
Melissa Metz <110804486+melissametz@users.noreply.github.com>
2023-07-05 20:28:10 -0700

    Fourth Commit - Updated ReadMe.md and gif

--- test/click-counter.test.js
+++ test/click-counter.test.js
@@ -6,8 +6,10 @@ beforeEach(() => {
 });
 
 // Show logs from the page inside labeled container
-const onPageConsole = msg => {
-  console.log(`<LOG::page console.${msg.type()}>${msg.text().replace(/\n/g, "<:LF:>")}`);
+const onPageConsole = (msg) => {
+  console.log(
+    `<LOG::page console.${msg.type()}>${msg.text().replace(/\n/g, "<:LF:>")}`
+  );
 };
 
 describe("Click Counter", () => {
@@ -15,43 +17,49 @@ describe("Click Counter", () => {
     if (!page.listeners("console").includes(onPageConsole)) {
       page.on("console", onPageConsole);
     }
-    
-    await page.goto(baseURL, {waitUntil: "load"});
+
+    await page.goto(baseURL, { waitUntil: "load" });
   });
-  
+
   it("should have an element with the class '.counter'", async () => {
-    const counterElem = await page.evaluate(() => document.querySelector(".counter"));
+    const counterElem = await page.evaluate(() =>
+      document.querySelector(".counter")
+    );
     expect(counterElem).not.toBeNull();
   });
-  
+
   it("should have an element with the class '.increment'", async () => {
-    const incElem = await page.evaluate(() => document.querySelector(".increment"));
+    const incElem = await page.evaluate(() =>
+      document.querySelector(".increment")
+    );
     expect(incElem).not.toBeNull();
   });
-  
+
   it("should have an element with the class '.decrement'", async () => {
-    const decElem = await page.evaluate(() => document.querySelector(".decrement"));
+    const decElem = await page.evaluate(() =>
+      document.querySelector(".decrement")
+    );
     expect(decElem).not.toBeNull();
   });
-  
+
   it("should have a default value of 0", async () => {
     await page.waitForSelector(".counter");
     const counterElem = await page.$(".counter");
     expect(counterElem).not.toBeNull();
-    const value = await page.evaluate(el => el.textContent, counterElem);
+    const value = await page.evaluate((el) => el.textContent, counterElem);
     expect(value).toEqual("0");
   });
-  
+
   it("should increase the counter by 1 when '.increment' is clicked", async () => {
     const incElem = await page.$(".increment");
     expect(incElem).not.toBeNull();
     await incElem.click();
     const counterElem = await page.$(".counter");
     expect(counterElem).not.toBeNull();
-    const value = await page.evaluate(el => el.textContent, counterElem);
+    const value = await page.evaluate((el) => el.textContent, counterElem);
     expect(value).toEqual("1");
   });
-  
+
   it("should decrease the counter by 1 when '.decrement' is clicked", async () => {
     await page.waitForSelector(".decrement");
     const decElem = await page.$(".decrement");
@@ -60,7 +68,7 @@ describe("Click Counter", () => {
     await page.waitForSelector(".counter");
     const counterElem = await page.$(".counter");
     expect(counterElem).not.toBeNull();
-    const value = await page.evaluate(el => el.textContent, counterElem);
+    const value = await page.evaluate((el) => el.textContent, counterElem);
     expect(value).toEqual("-1");
   });
 });
